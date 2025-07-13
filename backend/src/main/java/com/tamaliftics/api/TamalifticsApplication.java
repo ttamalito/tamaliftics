564ea5a5537b949812d105acc5325e9a678489ee
Jose Gonzalez <coding.tamalito@gmail.com>
2025-07-13 04:14:45 +0200

    Initial Commit

---
+++ backend/src/main/java/com/tamaliftics/api/TamalifticsApplication.java
@@ -0,0 +1,12 @@
+package com.tamaliftics.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamalifticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TamalifticsApplication.class, args);
+    }
+}
